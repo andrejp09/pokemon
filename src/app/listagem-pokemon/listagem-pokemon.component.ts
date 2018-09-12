d2791f5f7a1d562943eb91cc2c2c82b4d8daa5b2
AndreJuliano <andrejp09@gmail.com>
2018-09-11 23:16:01 -0300

    correções do front

--- src/app/listagem-pokemon/listagem-pokemon.component.ts
+++ src/app/listagem-pokemon/listagem-pokemon.component.ts
@@ -1,57 +1,53 @@
 import {Component, OnInit} from '@angular/core';
 import {ListagemPokemonService} from '../services/listagem-pokemon.service';
 import {ResponsePokemon} from '../models/response-pokemon';
-import {MatDialog} from '@angular/material';
 
 
 @Component({
-  selector: 'app-listagem-pokemon',
-  templateUrl: './listagem-pokemon.component.html',
-  styleUrls: ['./listagem-pokemon.component.css']
+    selector: 'app-listagem-pokemon',
+    templateUrl: './listagem-pokemon.component.html',
+    styleUrls: ['./listagem-pokemon.component.css']
 })
 export class ListagemPokemonComponent implements OnInit {
-  interval = {
-    limit: 6,
-    offset: 0
-  };
-  page = 1;
-  listaPokemom: ResponsePokemon;
+    interval = {
+        limit: 6,
+        offset: 0
+    };
+    page = 1;
+    listaPokemom: ResponsePokemon = new ResponsePokemon();
 
 
-  constructor(private service: ListagemPokemonService, public dialog: MatDialog) {
-  }
+    constructor(private service: ListagemPokemonService) {
+    }
 
 
-  ngOnInit() {
-    this.getPokemons();
-  }
+    ngOnInit() {
+        this.getPokemons();
+    }
 
-  changePage(newPage) {
-    this.interval.offset = (newPage - 1) * this.interval.limit;
-    this.getPokemons();
-  }
+    changePage(newPage) {
+        this.interval.offset = (newPage - 1) * this.interval.limit;
+        this.getPokemons();
+    }
 
-  getPokemons() {
-    this.service.getList(this.interval)
-      .then((value) => {
-        this.listaPokemom = value;
-        this.listaPokemom.results.forEach(item => {
-          this.getPokemonByName(item);
-        });
-      });
-  }
+    getPokemons() {
+        this.service.getList(this.interval)
+            .then((value) => {
+                this.listaPokemom = value;
+                this.listaPokemom.results.forEach(item => {
+                    this.getPokemonByName(item);
+                });
+            });
+    }
 
-  public getPokemonByName(pokemon) {
-    return new Promise((resolve, reject) => {
-      this.service.getPokemonByName(pokemon.name)
-        .then(
-          (value) => {
-            pokemon.urlFoto = value.sprites['front_default'];
-            return pokemon;
-          }
-        );
-
-    });
-  }
+    public getPokemonByName(pokemon) {
+        return this.service.getPokemonByName(pokemon.name)
+            .then(
+                (value) => {
+                    pokemon.urlFoto = value.sprites['front_default'];
+                    return pokemon;
+                }
+            );
+    }
 
 }

9376a98ca7fdfbefef76919e560632d6fb2a0343
AndreJuliano <andrejp09@gmail.com>
2018-09-11 23:30:42 -0300

    ajustando para integrar com heroku

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/dist/demo-deploy'));
 
 app.get('*', function(req,res) {
     // Replace the '/dist/<to_your_project_name>/index.html'
-    res.sendFile(path.join(__dirname+ '/dist/pokemon/index.html'));
+    res.sendFile(path.join(__dirname+ '/dist/desafio-pokemon/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
